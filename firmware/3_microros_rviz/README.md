e90a89ba4fb91c51d4b7b10a4e645cf8bb2dad82
Saish Karole <72208314+NachtSpyder04@users.noreply.github.com>
2024-03-30 21:24:15 +0530

    [ ROS2 Porting ] : Successful port of rosserial_rviz to microros_rviz (#52)

--- firmware/3_microros_rviz/README.md
+++ firmware/3_microros_rviz/README.md
@@ -1,40 +1,57 @@
-# ROSSERIAL RVIZ
+# MICRO-ROS RVIZ
 
-This example describe how to establish the rosserial connection between rviz and ESP32
+This example describe how to establish the micro-ROS connection between RViz and ESP32
 
 ## Steps to follow
 
-* Start the ROS Subscriber by executing this command in catkin workspace
+* Navigate to micro-ROS workspace (for example:- `microros_ws`) 
+    * ```cd microros_ws/``` (or name of your workspace directory)
+
+* Start and run the micro-ROS agent on your local system by executing following commands:
 
 ```bash
-rosrun rosserial_python serial_node.py tcp
+colcon build
+source install/setup.bash
+ros2 run micro_ros_agent micro_ros_agent udp4 --port 8888
 ```
 
-* Open another terminal and Configure the ros_control
-    * ``` cd firmware/rosserial_rviz ```
+
+
+* Open another terminal and source the ESP-IDF environment
+    * ``` cd MARIO/firmware/3_microros_rviz ```
+    * ```$IDF_PATH/export.sh``` (or ```get_idf``` if you have set up the alias in your `.bashrc` file)
+    
+* Configure the micro-ROS settings 
     * ```idf.py menuconfig```
-    * Inside `component config > MARIO-Rosserial`, Set Wifi ssid and password, Set the IP address (can be found by `ifconfig` command)
+    * inside `micro-ROS Settings` Set up WiFi configuration (SSID and Password) and your IP address in micro-ROS Agent IP (can be found by `ifconfig` command)
+
+**Note** If it shows the error `ifconfig: command not found`, install the package `net-tools` by following command:
+
+```bash
+sudo apt install net-tools
+```
 
 * Build and Flash the code 
     * ```idf.py build```
-    * ```idf.py -p PORT flash```
+    * ```idf.py -p PORT flash``` (For example: ```idf.py -p /dev/ttyUSB0 flash``` )
 
-* In another terminal Start the ROS Core service by running the following command
-    * ```roscore```
 
-* In another terminal Start the ROS Publisher by executing the relevant python script: (e.g. angle -> coordinates)
+
+
+* In another terminal, Navigate to the ROS2 workspace and Start the Publisher by executing the relevant python script after sourcing the environment
+(Make sure you have previously built the folder by executing `colcon build` command with 3_simulation_rviz in the src folder):
 
 ```bash
-rosrun simulation_rviz pub_try1.py
+cd ros2_ws/
+source install/setup.bash
+ros2 run simulation_rviz rviz.py
 ```
-In the same way publisher_inverse_kinematices.py can be run.
+
 
 **Note**:- If it shows the error that script is not executable, run the following command in the directory where the script is present.
 
 ```bash
-sudo chmod +x pub_try1.py
+sudo chmod +x rviz.py
 ```
 
 * Enter the angles on the publisher side and voila! See your manipulator moving!
-
-* Demo 
---
+++ firmware/3_microros_rviz/main/main.c
@@ -0,0 +1,199 @@
+/*
+MIT License
+
+Copyright (c) 2020 Society of Robotics and Automation
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+#include <string.h>
+#include <stdio.h>
+#include <unistd.h>
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_log.h"
+#include "esp_system.h"
+#include "sra_board.h"
+#include <uros_network_interfaces.h>
+#include <rcl/rcl.h>
+#include <rcl/error_handling.h>
+#include <sensor_msgs/msg/joint_state.h>
+#include <rclc/rclc.h>
+#include <rclc/executor.h>
+
+#ifdef CONFIG_MICRO_ROS_ESP_XRCE_DDS_MIDDLEWARE
+#include <rmw_microros/rmw_microros.h>
+#endif
+
+#define pi 3.141592653589
+
+//defined macros
+#define RCCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){printf("Failed status on line %d: %d. Aborting.\n",__LINE__,(int)temp_rc);vTaskDelete(NULL);}}
+#define RCSOFTCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){printf("Failed status on line %d: %d. Continuing.\n",__LINE__,(int)temp_rc);}}
+#define ARRAY_LEN 200
+#define JOINT_DOUBLE_LEN 5
+
+//Declaring variables with their respective data types
+rcl_subscription_t subscriber;
+sensor_msgs__msg__JointState recv_msg;
+char test_array[ARRAY_LEN];
+
+// structs to set up servo configurations
+servo_config servo_a = {
+	.servo_pin = SERVO_A,
+	.min_pulse_width = CONFIG_SERVO_A_MIN_PULSEWIDTH,
+	.max_pulse_width = CONFIG_SERVO_A_MAX_PULSEWIDTH,
+	.max_degree = CONFIG_SERVO_A_MAX_DEGREE,
+	.mcpwm_num = MCPWM_UNIT_0,
+	.timer_num = MCPWM_TIMER_0,
+	.gen = MCPWM_OPR_A,
+};
+
+servo_config servo_b = {
+	.servo_pin = SERVO_B,
+	.min_pulse_width = CONFIG_SERVO_B_MIN_PULSEWIDTH,
+	.max_pulse_width = CONFIG_SERVO_B_MAX_PULSEWIDTH,
+	.max_degree = CONFIG_SERVO_B_MAX_DEGREE,
+	.mcpwm_num = MCPWM_UNIT_0,
+	.timer_num = MCPWM_TIMER_0,
+	.gen = MCPWM_OPR_B,
+};
+
+servo_config servo_c = {
+	.servo_pin = SERVO_C,
+	.min_pulse_width = CONFIG_SERVO_C_MIN_PULSEWIDTH,
+	.max_pulse_width = CONFIG_SERVO_C_MAX_PULSEWIDTH,
+	.max_degree = CONFIG_SERVO_C_MAX_DEGREE,
+	.mcpwm_num = MCPWM_UNIT_0,
+	.timer_num = MCPWM_TIMER_1,
+	.gen = MCPWM_OPR_A,
+};
+
+servo_config servo_d = {
+	.servo_pin = SERVO_D,
+	.min_pulse_width = CONFIG_SERVO_D_MIN_PULSEWIDTH,
+	.max_pulse_width = CONFIG_SERVO_D_MAX_PULSEWIDTH,
+	.max_degree = CONFIG_SERVO_D_MAX_DEGREE,
+	.mcpwm_num = MCPWM_UNIT_0,
+	.timer_num = MCPWM_TIMER_1,
+	.gen = MCPWM_OPR_B,
+};
+
+
+// Callback function defined by user, automatically called whenever new message arrives on subscribed topic 
+void subscription_callback(const void * msgin)
+{
+    
+     const sensor_msgs__msg__JointState * msg = (const sensor_msgs__msg__JointState *)msgin;
+    
+    //To display data on terminal
+    printf("Received: %lf\n",  msg->position.data[0]);
+    printf("Received: %lf\n",  msg->position.data[1]);
+    printf("Received: %lf\n",  msg->position.data[2]);
+    printf("Received: %lf\n",  msg->position.data[3]);
+
+    //To control motors according to the data
+    set_angle_servo(&servo_a,msg->position.data[0]*180/pi);
+    set_angle_servo(&servo_b,msg->position.data[1]*180/pi);
+    set_angle_servo(&servo_c,msg->position.data[2]*180/pi);
+    set_angle_servo(&servo_d,msg->position.data[3]*180/pi);
+}
+
+void micro_ros_task(void * arg)
+{
+	//Initialization 
+  	memset(test_array,'z',ARRAY_LEN);
+	rcl_allocator_t allocator = rcl_get_default_allocator();
+	rclc_support_t support;
+
+	// Create init_options.
+	rcl_init_options_t init_options = rcl_get_zero_initialized_init_options();
+	RCCHECK(rcl_init_options_init(&init_options, allocator));
+
+#ifdef CONFIG_MICRO_ROS_ESP_XRCE_DDS_MIDDLEWARE
+	rmw_init_options_t* rmw_options = rcl_init_options_get_rmw_init_options(&init_options);
+
+	// Static Agent IP and port can be used instead of autodisvery.
+	RCCHECK(rmw_uros_options_set_udp_address(CONFIG_MICRO_ROS_AGENT_IP, CONFIG_MICRO_ROS_AGENT_PORT, rmw_options));
+	//RCCHECK(rmw_uros_discover_agent(rmw_options));
+#endif
+	// Setup support structure.
+	RCCHECK(rclc_support_init_with_options(&support, 0, NULL, &init_options, &allocator));
+
+	// Create node.
+	rcl_node_t node = rcl_get_zero_initialized_node();
+	RCCHECK(rclc_node_init_default(&node, "joint_state_sub", "", &support));
+
+	// Create subscriber.
+	RCCHECK(rclc_subscription_init_default(
+		&subscriber,
+		&node,
+		ROSIDL_GET_MSG_TYPE_SUPPORT(sensor_msgs, msg, JointState),
+		"/joint_states"));
+
+
+	// Create executor.
+	rclc_executor_t executor = rclc_executor_get_zero_initialized_executor();
+	RCCHECK(rclc_executor_init(&executor, &support.context, 2, &allocator));
+	unsigned int rcl_wait_timeout = 1000;   // in ms
+	RCCHECK(rclc_executor_set_timeout(&executor, RCL_MS_TO_NS(rcl_wait_timeout)));
+
+	RCCHECK(rclc_executor_add_subscription(&executor, &subscriber, &recv_msg, &subscription_callback, ON_NEW_DATA));
+
+	// Spin forever.
+	rosidl_runtime_c__String string_buffer[JOINT_DOUBLE_LEN];
+	recv_msg.name.data = string_buffer;
+	recv_msg.name.size = 0;
+	recv_msg.name.capacity = JOINT_DOUBLE_LEN;
+
+	for(int i = 0; i < JOINT_DOUBLE_LEN; i++){
+			recv_msg.name.data[i].data = (char*) malloc(ARRAY_LEN);
+			recv_msg.name.data[i].size = 0;
+			recv_msg.name.data[i].capacity = ARRAY_LEN;
+			recv_msg.position.data = (double*) malloc(ARRAY_LEN);
+			recv_msg.position.size = 0;
+			recv_msg.position.capacity = ARRAY_LEN;
+			recv_msg.velocity.data = (double*) malloc(ARRAY_LEN);
+			recv_msg.velocity.size = 0;
+			recv_msg.velocity.capacity = ARRAY_LEN;
+	}
+	rclc_executor_spin(&executor);
+
+	// Free resources.
+	RCCHECK(rcl_subscription_fini(&subscriber, &node));
+	RCCHECK(rcl_node_fini(&node));
+
+  	vTaskDelete(NULL);
+}
+
+void app_main(void)
+{
+#if defined(CONFIG_MICRO_ROS_ESP_NETIF_WLAN) || defined(CONFIG_MICRO_ROS_ESP_NETIF_ENET)
+    ESP_ERROR_CHECK(uros_network_interface_initialize());
+#endif
+
+    //pin micro-ros task in APP_CPU to make PRO_CPU to deal with wifi:
+    xTaskCreate(micro_ros_task,
+            "uros_task",
+            CONFIG_MICRO_ROS_APP_STACK,
+            NULL,
+            CONFIG_MICRO_ROS_APP_TASK_PRIO,
+            NULL);
+}
