fc0b70312d5ab2d1a6b9fb2ad4afddf0a2245ac3
purviyeshi <129578264+purviyeshi@users.noreply.github.com>
2024-04-13 14:35:12 +0545

    reconstruct struct according to servo.h

--- firmware/2_servo_sweep/main/main.c
+++ firmware/2_servo_sweep/main/main.c
@@ -1,26 +1,3 @@
-/*
-MIT License
-
-Copyright (c) 2024 Society of Robotics and Automation
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-*/
 #include "sra_board.h"
 
 #define TAG "MCPWM_SERVO_CONTROL"
@@ -30,9 +7,6 @@ servo_config servo_a = {
 	.min_pulse_width = CONFIG_SERVO_A_MIN_PULSEWIDTH,
 	.max_pulse_width = CONFIG_SERVO_A_MAX_PULSEWIDTH,
 	.max_degree = CONFIG_SERVO_A_MAX_DEGREE,
-	.mcpwm_num = MCPWM_UNIT_0,
-	.timer_num = MCPWM_TIMER_0,
-	.gen = MCPWM_OPR_A,
 };
 
 servo_config servo_b = {
@@ -40,9 +14,6 @@ servo_config servo_b = {
 	.min_pulse_width = CONFIG_SERVO_B_MIN_PULSEWIDTH,
 	.max_pulse_width = CONFIG_SERVO_B_MAX_PULSEWIDTH,
 	.max_degree = CONFIG_SERVO_B_MAX_DEGREE,
-	.mcpwm_num = MCPWM_UNIT_0,
-	.timer_num = MCPWM_TIMER_0,
-	.gen = MCPWM_OPR_B,
 };
 
 servo_config servo_c = {
@@ -50,9 +21,6 @@ servo_config servo_c = {
 	.min_pulse_width = CONFIG_SERVO_C_MIN_PULSEWIDTH,
 	.max_pulse_width = CONFIG_SERVO_C_MAX_PULSEWIDTH,
 	.max_degree = CONFIG_SERVO_C_MAX_DEGREE,
-	.mcpwm_num = MCPWM_UNIT_0,
-	.timer_num = MCPWM_TIMER_1,
-	.gen = MCPWM_OPR_A,
 };
 
 servo_config servo_d = {
@@ -60,9 +28,6 @@ servo_config servo_d = {
 	.min_pulse_width = CONFIG_SERVO_D_MIN_PULSEWIDTH,
 	.max_pulse_width = CONFIG_SERVO_D_MAX_PULSEWIDTH,
 	.max_degree = CONFIG_SERVO_D_MAX_DEGREE,
-	.mcpwm_num = MCPWM_UNIT_0,
-	.timer_num = MCPWM_TIMER_1,
-	.gen = MCPWM_OPR_B,
 };
 
 #ifdef CONFIG_ENABLE_OLED
